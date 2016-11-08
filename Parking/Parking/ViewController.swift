a567c22fb6a7a91e40c0125d05861305e74231d0
Randy Lina <randylina@Randys-MacBook.local>
2016-11-08 13:22:58 -0800

    Added color

--- Parking/Parking/ViewController.swift
+++ Parking/Parking/ViewController.swift
@@ -16,17 +16,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var name: UILabel!
     
     @IBOutlet weak var descrip: UILabel!
-
+    
+    @IBOutlet weak var enternameD: UILabel!
+    
     var name2:String = ""
+    
+    @IBOutlet weak var signin: UIButton!
+    
+    
+    @IBOutlet weak var entername: UITextField!
+    
+    
+    func output() {
+        name2 = "TESTING PRINT IN THE OUTPUT BELOW"
+        print(name2)
+    }
+    
+    func initial() {
+        label.text = "CSUCI Parking"
+        name.text = "Ralph & Evan"
+        descrip.text = "Here is our parking app"
+        enternameD.text = "Enter name"
+    }
+    
+    
+    func backColor() {
+        //self.view.backgroundColor = UIColor.grayColor()
+        self.view.backgroundColor = UIColor(red: (240/255)*2, green: (230/255)*2, blue: (200/255)*2, alpha: 0.5)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        name2 = "Ralph Lina"
-        print(name2)
-        label.text = "CSUCI Parking"
-        name.text = "Ralph & Evan"
-        descrip.text = "Here is our parking app"
+        output()
+        initial()
+        backColor()
+
     }
 
     override func didReceiveMemoryWarning() {
